bf2013469bf4a3a4dc0d8860438368973d6ba59d
Lynx <you@example.com>
2020-03-12 17:08:47 +0300

    initial commit

---
+++ app/src/main/java/ru/test/example/Loader.java
@@ -0,0 +1,28 @@
+package ru.test.example;
+
+import android.content.Context;
+
+/**
+ * DON'T MODIFY THIS FILE
+ */
+public class Loader
+{
+    private Context context;
+
+    private boolean is_finish = false;
+
+    public Loader(Context context)
+    {
+        this.context = context;
+    }
+
+    public void load()
+    {
+        this.is_finish = true;
+    }
+
+    public boolean isFinish()
+    {
+        return this.is_finish;
+    }
+}
---
+++ app/src/main/java/ru/test/example/TheGameActivity.java
@@ -0,0 +1,19 @@
+package ru.test.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * DON'T RENAME THIS ACTIVITY
+ * YOU CAN CHANGE BODY IN IT
+ */
+public class TheGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_game);
+    }
+}
